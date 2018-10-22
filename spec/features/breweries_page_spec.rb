cd12321bd48972055f332ce9da663b2a326514cf
vaisanem <miikkamv@gmail.com>
2018-10-23 00:15:30 +0300

    tests fixed

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -4,24 +4,26 @@ describe "Breweries page" do
   it "should not have any before been created" do
     visit breweries_path
     expect(page).to have_content 'Breweries'
-    expect(page).to have_content 'Number of breweries: 0'
+    expect(page).to have_content 'Active breweries 0'
+    expect(page).to have_content 'Retired breweries 0'
   end
 
-  describe "when breweries exists" do
+  describe "when breweries exist" do
     before :each do
       # jotta muuttuja näkyisi it-lohkoissa, tulee sen nimen alkaa @-merkillä
       @breweries = ["Koff", "Karjala", "Schlenkerla"]
       year = 1896
       @breweries.each do |brewery_name|
-        FactoryBot.create(:brewery, name: brewery_name, year: year += 1)
+        FactoryBot.create(:brewery, name: brewery_name, year: year += 1, active: year % 2 == 0)
       end
 
       visit breweries_path
     end
 
-    it "lists the existing breweries and their total number" do
+    it "lists the existing breweries and their total number for both active and retired" do
     
-      expect(page).to have_content "Number of breweries: #{@breweries.count}"
+      expect(page).to have_content "Active breweries #{Brewery.active.count}"
+      expect(page).to have_content "Retired breweries #{Brewery.retired.count}"
     
       @breweries.each do |brewery_name|
         expect(page).to have_content brewery_name
